232473fed67562ca21202506745347d37e10b0c4
hamouchr <74318193+hamouchr@users.noreply.github.com>
2022-03-09 09:14:41 +0100

    Ver 2021_2022

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,27 +1,10 @@
 #include <ros/ros.h>
-#include <std_msgs/String.h>
 
 using namespace ros;
-using namespace std_msgs;
-
-// A COMPLETER ...
-// Préciser le type de message reçu
-void infoCallback(/*... msg*/) {
-
-  // A DECOMMENTER ...
-  // ROS_INFO("Info Listener : I heard msg: [%s]", msg.data.c_str());
-}
 
 int main(int argc, char **argv) {
   init(argc, argv, "listener_node");
   NodeHandle nh;
-  Subscriber info_sub;
-
-  // A COMPLETER...
-  // Préciser le topic
-  // info_sub = nh.subscribe("...", 1000, infoCallback);
-
-  spin();
 
   return 0;
 }
--- src/talker.cpp
+++ src/talker.cpp
@@ -1,53 +1,12 @@
-#include <iostream>
 #include <ros/ros.h>
-#include <sstream>
-#include <std_msgs/String.h>
 
 // Les namespaces
-using namespace std;
 using namespace ros;
-using namespace std_msgs;
-
-// prototypes des fonctions
-string int_to_str(int x);
 
 int main(int argc, char **argv) {
 
   init(argc, argv, "publisher_node");
   NodeHandle nh;
-  String msg;
-  Publisher info_pub;
 
-  // A COMPLETER...
-  // Préciser le topic et le type du message associé
-  // info_pub = nh.advertise<...>("...", 1000);
-
-  // A COMPLETER ...
-  // Préciser la bonne fréqunce de publication
-  Rate loop_rate(1);
-
-  while (ok()) {
-
-    // A Compléter ...
-    // Affectation du message
-
-    // A Compléter ...
-    // Publication du message
-
-    ROS_INFO("Publisher : %s", msg.data.c_str());
-
-    spinOnce();
-    loop_rate.sleep();
-  }
   return 0;
-}
-
-
-
-
-
-string int_to_str(int x) {
-  stringstream ss;
-  ss << x;
-  return ss.str();
 }
